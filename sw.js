70ed09e0e51505646ef9249a7580669e6a0862bc
RealHeelal <heelalalanazi@gmail.com>
2026-05-14 15:13:20 +0300

    Initial commit

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'car-wash-cache-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(c => c.addAll([
+      './car-wash-forecast.html',
+      './manifest.json'
+    ]))
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
